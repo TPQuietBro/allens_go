b58b636c1e3e60f18cb1e3e1351e1f1277646643
pongtang <pongtang@tencent.com>
2022-06-23 15:41:51 +0800

    实现点击按钮发起请求并返回结果

--- webdemo/main.go
+++ webdemo/main.go
@@ -1,18 +1,87 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main()  {
-	http.HandleFunc("/home", handleReq)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	// 利用gin框架解决cors 跨域问题
+	g := gin.Default()
+	g.Use(AllowAll())
+	g.GET("/home/getuser", func(ctx *gin.Context) {
+		res := make(map[string]string)
+		res["name"] = "allen"
+		json, err := json.Marshal(res)
+		if err != nil {
+			log.Fatal("invalid json")
+		}
+
+		ctx.String(200, string(json))
+	})
+	_ = g.Run(":8080")
+
+	// http.HandleFunc("/home", handleLogin)
+	// http.HandleFunc("/home/getuser", handleGetUser)
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
+
+func AllowAll() gin.HandlerFunc {
+	cfg := cors.Config{
+		AllowMethods: []string{"*"},
+		AllowHeaders: []string{"*"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}
+	cfg.AllowAllOrigins = true
+	return cors.New(cfg)
+}
+
+func handleGetUser(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	params := r.Form
+	fmt.Println(params);
+	for k, v := range params {
+		tempV := strings.Join(v, "");
+		fmt.Printf("k = %v, v = %v \n", k, tempV);
+		value, err := strconv.Atoi(tempV);
+		if err != nil {
+			log.Fatal("invalid value")
+		}
+		if value == 1001 {
+			res := make(map[string]string)
+			res["name"] = "allen"
+			json, err := json.Marshal(res)
+			if err != nil {
+				log.Fatal("invalid json")
+			}
+			fmt.Println(string(json))
+			// fmt.Fprintln(w, string(json))
+			w.Write([]byte(string(json)))
+		}
 	}
 }
 
-func handleReq(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello my go"))
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("hello my go"))
+	r.ParseForm()
+	params := r.Form
+	fmt.Println(params);
+	for k, v := range params {
+		fmt.Printf("k = %v, v = %v \n", k, strings.Join(v, ""));
+	}
+
+	fmt.Fprintln(w, "hello my go")
+	// fmt.Println(r.URL.Query())
 }
